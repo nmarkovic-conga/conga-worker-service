b9ccdcd99a9a97b9827ed9999022a7596eadbacd
nikola-96 <nikolaviki@hotmail.com>
2021-09-15 12:59:36 +0200

    updating version 3

--- congaworkerservice/congaworkerservice.py
+++ congaworkerservice/congaworkerservice.py
@@ -6,15 +6,16 @@ class WorkerService:
     __instance = None
 
     @staticmethod
-    def getInstance():
+    def getInstance(queue_url: str, exchange: str, exchange_type: str):
         if WorkerService.__instance == None:
-            WorkerService()
+            WorkerService(queue_url, exchange, exchange_type)
         return WorkerService.__instance
 
-    def __init__(self, queue_url: str):
+    def __init__(self, queue_url: str, exchange: str, exchange_type: str):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=queue_url))
         self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange, exchange_type=exchange_type)
         WorkerService.__instance = self
 
     def start_consuming(self, callback, queue_chanel: str) -> None:
@@ -23,13 +24,13 @@ class WorkerService:
             queue=queue_chanel, on_message_callback=callback, auto_ack=True)
         self.channel.start_consuming()
 
-    def push_to_channel(self, document_id: int, file_name: str, document_type_id: int, processing_options: int, queue_chanel: str, extension="") -> None:
+    def push_to_channel(self, file_name: str, document_type_id: int, processing_options: int, queue_chanel: str, exchange_type: str = '')  -> None:
         self.channel.queue_declare(queue=queue_chanel, durable=True)
         self.channel.basic_publish(
-            exchange='',
-            routing_key=queue_chanel,
-            body=json.dumps({"document_id": document_id, "file_name": file_name, "document_type_id": document_type_id,
-                             "processing_options": processing_options, "extension": extension}),
+            exchange=exchange_type,
+            routing_key='extracted.paragraphs.queue',
+            body=json.dumps({"file_name": file_name, "document_type_id": document_type_id,
+                             "processing_options": processing_options}),
             properties=pika.BasicProperties(
                 delivery_mode=2,
             )
