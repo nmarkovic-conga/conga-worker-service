e68be454ba772b58e6b5840a5da5c77bcc2f7cad
nmarkovic-conga <nmarkovic@conga.com>
2021-09-23 20:18:44 +0200

    version 0.0.5

--- congaworkerservice/congaworkerservice.py
+++ congaworkerservice/congaworkerservice.py
@@ -6,16 +6,15 @@ class WorkerService:
     __instance = None
 
     @staticmethod
-    def getInstance(queue_url: str, exchange: str, exchange_type: str):
+    def getInstance(queue_url: str):
         if WorkerService.__instance == None:
-            WorkerService(queue_url, exchange, exchange_type)
+            WorkerService(queue_url)
         return WorkerService.__instance
 
-    def __init__(self, queue_url: str, exchange: str, exchange_type: str):
+    def __init__(self, queue_url: str):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=queue_url))
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=exchange, exchange_type=exchange_type)
         WorkerService.__instance = self
 
     def start_consuming(self, callback, queue_chanel: str) -> None:
@@ -23,11 +22,11 @@ class WorkerService:
         self.channel.basic_consume(
             queue=queue_chanel, on_message_callback=callback, auto_ack=True)
         self.channel.start_consuming()
-        
-    def push_to_channel(self, document_id: int, file_name: str, document_type_id: int, processing_options: int, queue_chanel: str, extension="", exchange_type: str = "") -> None:
+
+    def push_to_channel(self, document_id: int, file_name: str, document_type_id: int, processing_options: int, queue_chanel: str, extension="") -> None:
         self.channel.queue_declare(queue=queue_chanel, durable=True)
         self.channel.basic_publish(
-            exchange=exchange_type,
+            exchange="",
             routing_key=queue_chanel,
             body=json.dumps({"document_id": document_id, "file_name": file_name, "document_type_id": document_type_id,
                              "processing_options": processing_options, "extension": extension}),
